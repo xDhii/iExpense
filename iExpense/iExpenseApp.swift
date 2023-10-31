4bd86c2aebf8d59473892007581ba19d6faef629
Adriano Araujo Valumin <adriano.valumin@outlook.com>
2023-10-31 20:38:20 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Adriano Valumin on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
